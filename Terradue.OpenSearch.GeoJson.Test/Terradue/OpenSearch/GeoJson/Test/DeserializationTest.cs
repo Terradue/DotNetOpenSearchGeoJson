eb5be38d40bfeecb5fccdc21bbfef172189b955c
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-06-15 16:07:43 +0200

    aligned to os 1.10
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.OpenSearch.GeoJson.Test/Terradue/OpenSearch/GeoJson/Test/DeserializationTest.cs
+++ Terradue.OpenSearch.GeoJson.Test/Terradue/OpenSearch/GeoJson/Test/DeserializationTest.cs
@@ -32,24 +32,7 @@ namespace Terradue.OpenSearch.GeoJson.Test {
 
             Assert.That(fc.FeatureResults.Count == 50);
 
-            AddinManager.Initialize ();
-            AddinManager.Registry.Update ();
-
-            OpenSearchEngine ose = new OpenSearchEngine();
-            ose.LoadPlugins();
-
-            GenericOpenSearchable entity = new GenericOpenSearchable(new OpenSearchUrl("http://sb-10-16-10-20.dev.terradue.int/sbws/wps/dcs-doris-ifg/0000012-150204105530785-oozie-oozi-W/results/search?format=json&count=3&startPage=&startIndex=1&q=&lang=&id="), ose);
-
-            var results = ose.Query(entity, new System.Collections.Specialized.NameValueCollection());
-
-            Assert.That(results.Result.Items.Count() == 3);
-
-            entity = new GenericOpenSearchable(new OpenSearchUrl("https://data.terradue.com/gs/catalogue/tepqw/gtfeature/search?format=json&uid=S1A_EW_RAW__0SDH_20150323T073730_20150323T073759_005156_0067FF_2E86"), ose);
-
-            results = ose.Query(entity, new System.Collections.Specialized.NameValueCollection());
-
-            Assert.That(results.Result.Items.Count() == 1);
-
+           
         }
     }
 }
