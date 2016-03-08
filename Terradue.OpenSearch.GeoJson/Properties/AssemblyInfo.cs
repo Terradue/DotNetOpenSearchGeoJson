7145aa15ac4d6537caf58b6c4ee3b19ddee78a05
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-03-08 10:07:28 +0100

    nuget4mono.extensions

--- Terradue.OpenSearch.GeoJson/Properties/AssemblyInfo.cs
+++ Terradue.OpenSearch.GeoJson/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 @{
     DotNetOpenSearchGeoJson Software Package
 
-    \xrefitem sw_version "Versions" "Software Package Version" 1.3.3
+    \xrefitem sw_version "Versions" "Software Package Version" 1.3.5
 
     \xrefitem sw_link "Links" "Software Package List" [DotNetOpenSearchGeoJson](https://github.com/Terradue/DotNetOpenSearchGeoJson)
 
@@ -24,6 +24,9 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
+using NuGet4Mono.Extensions;
+
+
 [assembly: AssemblyTitle("Terradue.OpenSearch.GeoJson")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -32,11 +35,11 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Terradue")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.3.4.*")]
-[assembly: AssemblyInformationalVersion("1.3.4")]
+[assembly: AssemblyAuthors("Emmanuel Mathot")]
+[assembly: AssemblyProjectUrl("https://github.com/Terradue/DotNetGeoJson")]
+[assembly: AssemblyLicenseUrl("https://github.com/Terradue/DotNetGeoJson/blob/master/LICENSE")]
+[assembly: AssemblyVersion("1.3.5.*")]
+[assembly: AssemblyInformationalVersion("1.3.5")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
