9c11263cc2b33cafbf27fae49ad89f5f977764f7
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2017-04-07 15:18:36 +0200

    bug fix identifier
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@terradue.com>

--- Terradue.OpenSearch.GeoJson.Test/Terradue/OpenSearch/GeoJson/Test/ToAtomTest.cs
+++ Terradue.OpenSearch.GeoJson.Test/Terradue/OpenSearch/GeoJson/Test/ToAtomTest.cs
@@ -7,14 +7,18 @@ using Terradue.OpenSearch.GeoJson.Result;
 using Terradue.OpenSearch.GeoJson.Extensions;
 using Terradue.OpenSearch.Request;
 using System.IO;
+using System.Linq;
 
-namespace Terradue.OpenSearch.GeoJson.Test {
+namespace Terradue.OpenSearch.GeoJson.Test
+{
 
     [TestFixture()]
-    public class ToAtomTest {
+    public class ToAtomTest
+    {
 
         [Test()]
-        public void FromLandsat8AtomTest() {
+        public void FromLandsat8AtomTest()
+        {
 
             var fs = new FileStream("../Samples/ASA_IM__0.json", FileMode.Open, FileAccess.Read);
 
@@ -25,6 +29,26 @@ namespace Terradue.OpenSearch.GeoJson.Test {
             var feed = AtomFeed.CreateFromOpenSearchResultCollection(fc);
 
             Assert.That(feed != null);
+
+        }
+
+        [Test()]
+        public void FromS1GRDJsonTest()
+        {
+
+            var fs = new FileStream("../Samples/S1_GRD.json", FileMode.Open, FileAccess.Read);
+
+            var fc = FeatureCollectionResult.DeserializeFromStream(fs);
+
+            fs.Close();
+
+            var feed = AtomFeed.CreateFromOpenSearchResultCollection(fc);
+
+            Assert.That(feed != null);
+
+            string identifier = feed.Items.First().ElementExtensions.ReadElementExtensions<string>("identifier", "http://purl.org/dc/elements/1.1/").First();
+
+            Assert.AreEqual("S1A_IW_GRDH_1SDV_20160731T181152_20160731T181219_012396_013552_7CC6", identifier);
 
         }
     }
